a44f2728a47cef8844b3ad1c2e617387349df607
thejacktan <59751623+thejacktan@users.noreply.github.com>
2020-08-01 03:40:34 -0600

    Fixed function typo, added error checks, added minor improvements, improved repo structure and added usage convenience.

--- src/functions.py
+++ src/functions.py
@@ -4,31 +4,40 @@ import instaloader
 import pandas as pd
 from datetime import datetime, date
 from pathlib import Path
+import sys
 
 
-def get_data(target_name, login_name):
+def get_data(target_name, login_name, login_pass):
     df_followees = pd.DataFrame(columns=['userid', 'username', 'full_name'])
     df_followers = pd.DataFrame(columns=['userid', 'username', 'full_name'])
     today = date.today()
 
     L = instaloader.Instaloader()
-    L.interactive_login(login_name)
+    L.login(user=login_name, passwd=login_pass)
 
-    profile = instaloader.Profile.from_username(L.context, target_name)
+    try:
+        target_profile = instaloader.Profile.from_username(L.context, target_name)
+    except Exception as e:
+        print(e)
+        print('Target profile does not exist! Please make sure ' + login_name + ' is not blocked by target profile!')
+        sys.exit(1)
 
-    if (profile.is_private == True) & (profile.followed_by_viewer == False):
+    # Seeing if we're able to access target profile data
+    if (target_profile.is_private == True) & (target_profile.followed_by_viewer == False):
         raise Exception('The target profile "' + target_name + '" is private! Account "' +
                         login_name + '" has not followed them!')
 
-    for followee in profile.get_followees():
-        temp_data = {'userid': [followee.userid], 'username': [followee.username], 'full_name': [followee.full_name]}
-        temp_df = pd.DataFrame(data=temp_data)
-        df_followees = df_followees.append(temp_df, ignore_index=True)
+    # Adding followers and followees
+    for followee in target_profile.get_followees():
+        df_followees = df_followees.append(
+            pd.DataFrame(data={'userid': [followee.userid], 'username': [followee.username],
+                               'full_name': [followee.full_name]}), ignore_index=True)
 
-    for follower in profile.get_followers():
-        temp_data = {'userid': [follower.userid], 'username': [follower.username], 'full_name': [follower.full_name]}
-        temp_df = pd.DataFrame(data=temp_data)
-        df_followers = df_followees.append(temp_df, ignore_index=True)
+    for follower in target_profile.get_followers():
+        df_followers = df_followers.append(
+            pd.DataFrame(data={'userid': [follower.userid], 'username': [follower.username],
+                               'full_name': [follower.full_name]}),
+            ignore_index=True)
 
     Path('../data').mkdir(exist_ok=True)
 
@@ -42,27 +51,32 @@ def get_data(target_name, login_name):
 
 
 def analyze_data(target_name, from_date='', to_date=''):
+    L = instaloader.Instaloader()
     my_files_all = os.listdir('../data/')
     my_files = copy.deepcopy(my_files_all)
 
+    # Filtering for files associated with target profile
     for file in my_files_all:
         if file[-(len(target_name) + 4): -4] != target_name:
             my_files.remove(file)
 
+    # Finding default "to" and "from" dates if arguments not given
     if (to_date == '') or (from_date == ''):
         default_to_date = datetime(1970, 1, 2)
         default_from_date = datetime(1970, 1, 2)
         for file in my_files:
             file_date = datetime.strptime(file[0:10], '%Y-%m-%d')
+
+            # Shifts both the "from" and "to" dates
             if file_date.timestamp() > default_to_date.timestamp():
                 default_from_date = default_to_date
                 default_to_date = file_date
-                # Feels so redundant lol
                 to_date_followee = str(default_to_date.date()) + '_followees_' + target_name
                 to_date_follower = str(default_to_date.date()) + '_followers_' + target_name
                 from_date_followee = str(default_from_date.date()) + '_followees_' + target_name
                 from_date_follower = str(default_from_date.date()) + '_followers_' + target_name
 
+            # Only shifts the "from" date
             elif (file_date.timestamp() > default_from_date.timestamp()) & \
                     (file_date.timestamp() != default_to_date.timestamp()):
                 default_from_date = file_date
@@ -73,48 +87,132 @@ def analyze_data(target_name, from_date='', to_date=''):
         if default_to_date == datetime(1970, 1, 2):
             raise Exception('No files found in "data" directory! Make sure file names follows proper formating!')
         if default_from_date == datetime(1970, 1, 2):
-            raise Exception('Only one day in "data" directory. Nothing to compare with!')
+            print('Only one day in "data" directory! Limited report can be generated')
 
+    # Taking manual input if given
     if to_date != '':
         assert len(to_date) == 10, '"to_date" argument only accepts "YYYY-MM-DD" as input!'
         assert type(to_date) == str, '"to_date" argument only accepts string inputs!'
+        default_to_date = datetime.strptime(to_date, '%Y-%m-%d')
         to_date_followee = to_date + '_followees_' + target_name
         to_date_follower = to_date + '_followers_' + target_name
-    else:
-        to_date = default_to_date.date()
 
     if from_date != '':
         assert len(from_date) == 10, '"from_date" argument only accepts "YYYY-MM-DD" as input!'
         assert type(from_date) == str, '"from_date" argument only accepts string inputs!'
+        default_from_date = datetime.strptime(from_date, '%Y-%m-%d')
         from_date_followee = from_date + '_followees_' + target_name
         from_date_follower = from_date + '_followers_' + target_name
-    else:
-        from_date = default_from_date.date()
 
-    # Actually reading the files
-    to_date_followee_data = pd.read_csv('../data/' + to_date_followee + '.csv')
-    to_date_follower_data = pd.read_csv('../data/' + to_date_follower + '.csv')
+    # Attempting to read data files from the specified dates
+    try:
+        to_date_followee_data = pd.read_csv('../data/' + to_date_followee + '.csv')
+    except:
+        print('Unable to find file: ' + to_date_followee + '.csv')
 
-    from_date_followee_data = pd.read_csv('../data/' + from_date_followee + '.csv')
-    from_date_follower_data = pd.read_csv('../data/' + from_date_follower + '.csv')
+    try:
+        to_date_follower_data = pd.read_csv('../data/' + to_date_follower + '.csv')
+    except:
+        print('Unable to find file: ' + to_date_follower + '.csv')
 
-    new_followers = list(set(to_date_follower_data['userid']) - set(from_date_follower_data['userid']))
-    past_followers = list(set(from_date_follower_data['userid']) - set(to_date_follower_data['userid']))
+    try:
+        from_date_followee_data = pd.read_csv('../data/' + from_date_followee + '.csv')
+    except:
+        print('Unable to find file: ' + from_date_followee + '.csv')
 
-    new_followees = list(set(to_date_followee_data['userid']) - set(from_date_followee_data['userid']))
-    past_followees = list(set(from_date_followee_data['userid']) - set(to_date_followee_data['userid']))
+    try:
+        from_date_follower_data = pd.read_csv('../data/' + from_date_follower + '.csv')
+    except:
+        print('Unable to find file: ' + from_date_follower + '.csv')
+
+    # Generating report as appropriate
+    deactivated_id = list()
+    deactivated = list()
+
+    try:
+        new_followers_id = list(set(to_date_follower_data['userid']) - set(from_date_follower_data['userid']))
+        new_followers = list(to_date_follower_data[to_date_follower_data['userid'].isin(new_followers_id)]['username'])
+    except:
+        new_followers = list()
+
+    try:
+        past_followers_id = list(set(from_date_follower_data['userid']) - set(to_date_follower_data['userid']))
+        for n in past_followers_id:
+            if n in deactivated_id:
+                past_followers_id.remove(n)
+                continue
+            else:
+                try:
+                    instaloader.Profile.from_id(L.context, n)
+                except Exception:
+                    past_followers_id.remove(n)
+                    deactivated_id.append(n)
+                    deactivated.append(
+                        from_date_follower_data[from_date_follower_data['userid'] == n]['username'].values[0])
+
+        past_followers = list(
+            from_date_follower_data[from_date_follower_data['userid'].isin(past_followers_id)]['username'])
+    except:
+        past_followers = list()
+
+    try:
+        new_followees_id = list(set(to_date_followee_data['userid']) - set(from_date_followee_data['userid']))
+        new_followees = list(to_date_followee_data[to_date_followee_data['userid'].isin(new_followees_id)]['username'])
+    except:
+        new_followees = list()
+
+    try:
+        past_followees_id = list(set(from_date_followee_data['userid']) - set(to_date_followee_data['userid']))
+        for n in past_followees_id:
+            if n in deactivated_id:
+                past_followees_id.remove(n)
+                continue
+            else:
+                try:
+                    instaloader.Profile.from_id(L.context, n)
+                except Exception:
+                    past_followees_id.remove(n)
+                    deactivated_id.append(n)
+                    deactivated.append(
+                        from_date_followee_data[from_date_followee_data['userid'] == n]['username'].values[0])
+
+        past_followees = list(
+            from_date_followee_data[from_date_followee_data['userid'].isin(past_followees_id)]['username'])
+    except:
+        past_followees = list()
+
+    try:
+        no_followback_id = list(set(to_date_followee_data['userid']) - set(to_date_follower_data['userid']))
+        no_followback = list(to_date_followee_data[to_date_followee_data['userid'].isin(no_followback_id)]['username'])
+    except:
+        no_followback = list()
+
+    try:
+        no_follow_id = list(set(to_date_follower_data['userid']) - set(to_date_followee_data['userid']))
+        no_follow = list(to_date_follower_data[to_date_follower_data['userid'].isin(no_follow_id)]['username'])
+    except:
+        no_follow = list()
 
     report_data = {
-        'New Followers': to_date_follower_data[to_date_follower_data['userid'].isin(new_followers)]['username'],
-        'Accounts Who Unfollowed You': from_date_follower_data[from_date_follower_data['userid'].isin(past_followers)]['username'],
-        'New Accounts You Follow': to_date_followee_data[to_date_followee_data['userid'].isin(new_followees)]['username'],
-        'Accounts You Unfollowed': from_date_followee_data[from_date_followee_data['userid'].isin(past_followees)]['username']}
+        ("New Followers of '" + target_name + "'"): new_followers,
+        ("Accounts Who Unfollowed '" + target_name + "'"): past_followers,
+        ("New Accounts Followed By '" + target_name + "'"): new_followees,
+        ("Accounts Unfollowed By '" + target_name + "'"): past_followees,
+        "Accounts Deactivated/Deleted": deactivated,
+        ("Accounts Who Don't Follow Back '" + target_name + "' (" + str(default_to_date.date()) + ")"): no_followback,
+        ("Accounts '" + target_name + "' Doesn't Follow Back (" + str(default_to_date.date()) + ")"): no_follow
+    }
 
-    report_df = pd.DataFrame(data=report_data)
+    report_df = pd.DataFrame.from_dict(data=report_data, orient='index').transpose()
 
     Path('../reports').mkdir(exist_ok=True)
 
-    report_df.to_csv(path_or_buf = ('../reports/' + str(from_date) + '_to_' + str(to_date) + '_' + target_name + '.csv'),
-                     index = False)
+    if default_from_date == datetime(1970, 1, 2):
+        report_df.to_csv(path_or_buf=('../reports/' + str(default_to_date.date()) + '_' + target_name + '.csv'),
+                         index=False)
+    else:
+        report_df.to_csv(path_or_buf=('../reports/' + str(default_from_date.date()) + '_to_' +
+                                      str(default_to_date.date()) + '_' + target_name + '.csv'),
+                         index=False)
 
     return
--- src/main_script.py
+++ src/main_script.py
@@ -5,14 +5,18 @@ default_pass = "456827913"
 
 my_target = str(input("Profile you want to analyze: "))
 print("You have selected profile handle: " + my_target)
-my_login = str(input("Profile you want to login with: "))
-print("You have selected to login as: " + my_login)
-my_start = str(input("Start date (YYYY-MM_DD), don't enter anything for default: "))
-print("You have selected start date: " + my_start)
-my_end = str(input("End date (YYYY-MM_DD), don't enter anything for default: "))
-print("You have selected start date: " + my_end)
+my_login = str(input("Profile you want to login with (leave blank for default): "))
 
-my_fun.get_data(target_name = my_target, login_name = my_login)
+if len(my_login) == 0:
+    my_login = default_name
+    my_pass = default_pass
+else:
+    my_pass = str(input("Password for " + my_login + " : "))
+
+my_start = str(input("Start date (YYYY-MM-DD), leave blank for default: "))
+my_end = str(input("End date (YYYY-MM-DD), leave blank for default: "))
+
+my_fun.get_data(target_name = my_target, login_name = my_login, login_pass = my_pass)
 my_fun.analyze_data(target_name = my_target, from_date = my_start, to_date= my_end)
 
 print('Done!')
